10f1251703e1a5cb3e3523bfb4e87334ef3b71e7
biniam <biniam@172.16.1.188>
2016-12-11 23:52:11 -0500

    This is what is changed.

--- HelloGit/src/com/git/hello/HelloGitDriver.java
+++ HelloGit/src/com/git/hello/HelloGitDriver.java
@@ -5,6 +5,7 @@ public class HelloGitDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to my first git project, seeing how it works!!");
+		System.out.print("Done!!");
 
 	}
 
